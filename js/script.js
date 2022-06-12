80af28b2f34fc4d509b6782d16c2f4ab15795ce8
Enzo <EnzoPiccolo7>
2022-06-12 13:05:10 +0200

    primo commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 
+    let img = document.querySelector('.btn');
+    img.addEventListener('click',esce);
+
+function esce(){
 let chiamata = fetch('../json/users.json').then(response => response.json()); 
 console.log(chiamata);
 
   chiamata.then(data => {
-    let utenti = document.querySelector('.contain');
+    let utenti = document.querySelector('.cont');
         data.forEach(ele => {
             let li = document.createElement('div');
             li.innerHTML =` 
-            
+
                     <div class="card" style="width: 18rem;">
                     <img src="${ele.profileURL}" class="card-img-top"></img>
-                    <div class="card-body">
+                    <div id="card" class="card-body">
                         <h5 class="card-title">${ele.username}</h5>
                         <p class="class-text">${ele.firstName} ${ele.lastName}</p>
                         <p class="class-text">Gender: ${ele.gender}</p>
@@ -19,9 +23,15 @@ console.log(chiamata);
                     </div>
                     </div>
             `;
-            let arrayut = new Array();
-            arrayut.push(ele);
-            console.log(arrayut[0]);
+            
             utenti.appendChild(li);
+            
         })
-})  
+
+    })  
+}
+/* img.addEventListener('dbclick',esce); */
+
+
+ 
+
